2c0dfcbc694726547ce300f74639dba9fa32d9b9
chenzhengyu <944989036@qq.com>
2020-12-08 12:26:00 +0800

    线程池测试类

---
+++ src/main/java/com/gzczy/concurrent/heima/f/ExecutorPoolTest.java
@@ -0,0 +1,95 @@
+package com.gzczy.concurrent.heima.f;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+
+/**
+ * @Description 线程池测试类
+ * @Author chenzhengyu
+ * @Date 2020-12-08 10:31
+ */
+@Slf4j(topic = "c.ExecutorPoolTest")
+public class ExecutorPoolTest {
+
+    public static void main(String[] args) throws Exception {
+        //ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        //extractedCachedPool(cachedThreadPool);
+
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(2);
+        extractedFixedPool(fixedThreadPool);
+    }
+
+    private static void extractedFixedPool(ExecutorService fixedThreadPool) throws InterruptedException, ExecutionException {
+        fixedThreadPool.submit(() -> {
+            log.info("start 1");
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info("end 1");
+        });
+
+        fixedThreadPool.invokeAny(Arrays.asList(
+                () -> {
+                    log.info("start 2");
+                    TimeUnit.MILLISECONDS.sleep(500);
+                    return "2";
+                },
+                () -> {
+                    log.info("start 3");
+                    TimeUnit.MILLISECONDS.sleep(1500);
+                    return "3";
+                }
+        ));
+
+        //内部实现直接打断
+        fixedThreadPool.shutdownNow();
+        log.info("is shutDown now ? {}", fixedThreadPool.isShutdown());
+    }
+
+    /**
+     * 测试 cachedThreadPool
+     *
+     * @param cachedThreadPool
+     * @throws InterruptedException
+     */
+    private static void extractedCachedPool(ExecutorService cachedThreadPool) throws InterruptedException {
+        List<Future<Object>> resultList = cachedThreadPool.invokeAll(Arrays.asList(
+                () -> {
+                    log.info("start 1");
+                    TimeUnit.SECONDS.sleep(1);
+                    return "1";
+                },
+                () -> {
+                    log.info("start 2");
+                    TimeUnit.MILLISECONDS.sleep(500);
+                    return "2";
+                },
+                () -> {
+                    log.info("start 3");
+                    TimeUnit.MILLISECONDS.sleep(1500);
+                    return "3";
+                }
+        ));
+
+        // 异步遍历get 数据
+        resultList.forEach(x -> {
+            try {
+                log.info("result - > {}", x.get().toString());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        });
+
+        cachedThreadPool.shutdown();
+        //shutdown后再提交触发拒绝策略
+        cachedThreadPool.submit(() -> {
+            TimeUnit.SECONDS.sleep(1);
+            return "after shutdown test";
+        });
+    }
+}
